790a70caa5677138d59acbc5f88889b832b3a54f
Plüss Roland <roland@rptd.ch>
2024-10-04 11:12:24 +0200

    updated copyright

--- linux/site_scons/site_tools/runExternalCommand.py
+++ linux/site_scons/site_tools/runExternalCommand.py
@@ -1,19 +1,18 @@
-import sys, re
 from os import system
 
 # Provides running calls protected by a synchronization lock to ensure only one call runs
 # at the same time. A typical use for this is wrapping os.system() calls.
-# 
+#
 # Required for certain build commands required to run non-threaded even if the user has
 # run external command
-# 
+#
 # A typical use is like this:
-# 
+#
 # env.Tool('runExternalCommand')
-# 
+#
 # env.RunExternalCommand('binary argument')  # using os.system()
 # env.RunExternalCommand(['binary1 argument1', 'binary2 argument2'])  # using os.system()
-# 
+#
 # Calls return True on success or False otherwise. Raising exceptions is allowed
 
 # SCons Tools required this method to be present. It is called if exists() returns true
